f63aaba84108a63b614eee59b37f4b9f4c5e9456
GAweb77 <giuseppe.argento@gbrain.it>
2023-11-29 10:45:43 +0100

    fix

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,15 +1,14 @@
-
-//@ts-nocheck
+// Import necessary modules
+import { NextApiRequest, NextApiResponse } from 'next';
 import Stripe from 'stripe';
 
-
+// Create a new instance of the Stripe class with your API key
 const stripe = new Stripe('sk_test_51Gvs9lG8Eb6lvUjg4RzBs2fuVZPaHU1RQBbuGNq4F1gRhdzLRdFFAGEjrf2vboEMg866Hv392EXdzzcEkOlcaeyh00TymnI58x');
 
-
-export async function POST(req: Request) {
-
- 
-      
+// Define the handler function for the API endpoint
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    if (req.method === 'POST') {
       const { lineItems } = req.body;
 
       // Create a new checkout session
@@ -22,6 +21,13 @@ export async function POST(req: Request) {
       });
 
       res.status(200).json({ id: session.id });
-    
+    } else {
+      res.setHeader('Allow', ['POST']);
+      res.status(405).end('Method Not Allowed');
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 }
 
